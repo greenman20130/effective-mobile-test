481f54e18ebd1d79f586879c90f248327915963c
Алексей Шелестов <63450993+greenman20130@users.noreply.github.com>
2024-11-22 10:37:11 +0300

    create book.py
    Created new file book.py and class Book

---
+++ src/book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, id, title, author, year, status):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
